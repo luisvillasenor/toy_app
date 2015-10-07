6f516635c5860cdfdb85cf84d5e663b47d60ce30
Luigi <luisvillasenor@yahoo.com>
2015-10-07 16:23:39 +0000

    Agrega validaciones campos vacios

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,5 @@ class Micropost < ActiveRecord::Base
   # Uno o varios microposts peertenecen  un solo usuario
   belongs_to :user
   # Hacemos una restriccion y validacion del tamaño el post a 140 caracteres y evitar que el campo este en blanco
-  validates :content, length: { maximum: 140 },
-                                presence: true
+  validates :content, length: { maximum: 140 }, presence: true
 end
